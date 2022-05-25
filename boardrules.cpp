df21f23b320621e49ca733e5d6d8372740402be2
mdelopo <87904792+mdelopo@users.noreply.github.com>
2022-05-25 19:48:23 +0300

    Progress 1

--- boardrules.cpp
+++ boardrules.cpp
@@ -4,10 +4,79 @@
 // TODO: Implement here the method pieceCanBePlaced and computeScore of Board
 // Do not implement any other methods of Board (as they are already implemented in file board.cpp)
 
-bool Board::pieceCanBePlaced(Piece* piece, int x, int y) {
-    return true;
+bool Board::pieceCanBePlaced(Piece* piece, int x, int y)
+{
+    bool canBePlaced = true;
+    if(x<0 || x+piece->getSizeX()>13) return false;
+    if(y<0 || y+piece->getSizeY()>13) return false;
+
+    if(playerHasPlacedNoPieces(piece->getPlayer())) //proti kinisi
+    {
+        int firstX, firstY;
+        if(piece->getPlayer()=='#')
+        {
+            firstX=4;
+            firstY=4;
+        }
+        else if(piece->getPlayer()=='O')
+        {
+            firstX=9;
+            firstY=9;
+        }
+        for(int i=0; i<piece->getSizeX(); i++)
+        {
+            for(int j=0; j<piece->getSizeY(); j++)
+            {
+                if(piece->squareHasPiece(i,j)==true && x+i==firstX && y+j==firstY) return true;
+            }
+        }
+
+    }
+
+    for(int i=0; i<piece->getSizeX(); i++) //
+    {
+        for(int j=0; j<piece->getSizeY(); j++)
+        {
+            if(hasPiece(x+i,y+j)==true && piece->squareHasPiece(i,j)==true) return false;
+        }
+    }
+
+    canBePlaced=false;
+    for(int i=0; i<piece->getSizeX(); i++) //
+    {
+        for(int j=0; j<piece->getSizeY(); j++)
+        {
+            if(piece->squareHasPiece(i,j))
+            {
+                if(squareBelongsToPlayer(x+i-1,y+j-1,piece->getPlayer())) canBePlaced = true;
+                if(squareBelongsToPlayer(x+i-1,y+j+1,piece->getPlayer())) canBePlaced = true;
+                if(squareBelongsToPlayer(x+i+1,y+j-1,piece->getPlayer())) canBePlaced = true;
+                if(squareBelongsToPlayer(x+i+1,y+j+1,piece->getPlayer())) canBePlaced = true;
+
+
+                if(squareBelongsToPlayer(x+i-1,y+j,piece->getPlayer())) canBePlaced = false;
+                if(squareBelongsToPlayer(x+i+1,y+j,piece->getPlayer())) canBePlaced = false;
+                if(squareBelongsToPlayer(x+i,y+j-1,piece->getPlayer())) canBePlaced = false;
+                if(squareBelongsToPlayer(x+i,y+j+1,piece->getPlayer())) canBePlaced = false;
+            }
+        }
+    }
+    return canBePlaced;
 }
 
-int Board::computeScore(Player* player){
-    return 0;
+int Board::computeScore(Player* player)
+{
+    int score = 0;
+    for(int i=0; i<14; i++){
+        for(int j=0; j<14;j++){
+            if(squareBelongsToPlayer(i,j,player->getSymbol())) score++;
+        }
+    }
+
+    if(player->getNumberOfAvailablePieces()==0) score+=15;
+
+    Piece* lastPiecePlayed = getLastPiecePlayedByPlayer(player);
+    if(lastPiecePlayed->getId()==0) score+=5;
+
+    return score;
 }
--- player.cpp
+++ player.cpp
@@ -3,3 +3,90 @@
 #include "player.h"
 
 // TODO: Implement here the methods of Player and all derived classes
+
+Player::Player(int id)
+{
+    this->id=id;
+    if(id==0)
+    {
+        name = "Player 1";
+        symbol = '#';
+    }
+    else if(id==1)
+    {
+        name = "Player 2";
+        symbol = 'O';
+    }
+    numPieces = 21;
+    pieces = new Piece*[21];
+    createPieces(); //asteraki
+}
+
+Player::~Player(){
+    for(int i=0; i<21; i++)
+    {
+        delete pieces[i];
+    }
+    delete pieces;
+}
+
+int Player::getId()
+{
+    return id;
+}
+
+char Player::getSymbol()
+{
+    return symbol;
+}
+
+string Player::getName()
+{
+    return name;
+}
+
+Piece* Player::getPiece(int index)
+{
+    return pieces[index];
+}
+
+int Player::getNumberOfPlacedPieces()
+{
+    return 21-numPieces;
+}
+
+int Player::getNumberOfAvailablePieces()
+{
+    return numPieces;
+}
+
+//HumanPlayer
+
+HumanPlayer::HumanPlayer(int id):Player(id)
+{
+}
+
+HumanPlayer::HumanPlayer(int id, string name):Player(id)
+{
+    this->name = name;
+}
+
+//Computerplayer
+
+int ComputerPlayer::getRandomPieceId()
+{
+    return rand()%21;
+}
+
+Orientation ComputerPlayer::getRandomOrientation()
+{
+    return (Orientation)(rand()%4); // Rather ugly, but correct :D
+}
+
+Flip ComputerPlayer::getRandomFlip()
+{
+    return (Flip)(rand()%2);
+}
+
+ComputerPlayer::ComputerPlayer(int id):Player(id){}
+
